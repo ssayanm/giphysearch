4ed7deaf9d430375850ec833cc7a55105c1f2440
Sayan Mukherjee <hello@sayanmukherjee.com>
2019-10-21 10:29:49 +0530

    Updates

---
+++ precache-manifest.1f5a14dd2e665c55c05f691d8de979f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f341e2fe014a1f2831ba380421d5e9c",
+    "url": "/giphysearch/index.html"
+  },
+  {
+    "revision": "2c00ca121f7949eeb4c5",
+    "url": "/giphysearch/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "410e6ac30b590084f4cb",
+    "url": "/giphysearch/static/css/main.53752d27.chunk.css"
+  },
+  {
+    "revision": "2c00ca121f7949eeb4c5",
+    "url": "/giphysearch/static/js/2.03367fa3.chunk.js"
+  },
+  {
+    "revision": "410e6ac30b590084f4cb",
+    "url": "/giphysearch/static/js/main.5198813e.chunk.js"
+  },
+  {
+    "revision": "c3b3cfa6ae575abc4a3f",
+    "url": "/giphysearch/static/js/runtime~main.2ce7761d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/giphysearch/static/media/SEGA.12a3db8b.woff"
+  }
+]);
