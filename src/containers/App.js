a067a82a20fef69cb99bbc684e3c1d690981da32
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-10-17 07:21:47 +0530

    updated app.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,65 +1,65 @@
-import React, { Component } from 'react';
-import GifList from '../components/GifList';
-import DisplayGif from '../components/DisplayGif';
-import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
-import ErrorBoundary from '../components/ErrorBoundary';
-import './App.css';
+import React, { Component } from "react";
+import GifList from "../components/GifList";
+import DisplayGif from "../components/DisplayGif";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+import ErrorBoundary from "../components/ErrorBoundary";
+import "./App.css";
 
-const API_URL = 'http://api.giphy.com/v1/gifs/search?q=';
-const API_KEY = '&api_key=vymc5XlA7eUV2Idx6Qv6nz61ltpL16sT';
-const LIMIT ='&limit=10'
-const DEFAULT_QUERY = 'ryan';
+const API_URL = "http://api.giphy.com/v1/gifs/search?q=";
+const API_KEY = "&api_key=vymc5XlA7eUV2Idx6Qv6nz61ltpL16sT";
+const LIMIT = "&limit=10";
+const DEFAULT_QUERY = "ryan";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-        gifs: [],
-        fgifs:[],
-        searchfield:''
-    }
+      gifs: [],
+      fgifs: [],
+      searchfield: "",
+    };
     //this.onSearchChange = this.onSearchChange.bind(this);
   }
 
-componentDidMount() {
+  componentDidMount() {
     fetch(API_URL + DEFAULT_QUERY + API_KEY + LIMIT)
-      .then(response => response.json())
-      .then(items => {
-        this.setState({ fgifs: items.data})});
-      console.log(this.state.fgifs);
+      .then((response) => response.json())
+      .then((items) => {
+        this.setState({ fgifs: items.data });
+      });
   }
 
   onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
+    this.setState({ searchfield: event.target.value });
     fetch(API_URL + event.target.value + API_KEY + LIMIT)
-      .then(response => response.json())
-      .then(items => {
-        this.setState({ gifs: items.data})});
-  }
+      .then((response) => response.json())
+      .then((items) => {
+        this.setState({ gifs: items.data });
+      });
+  };
 
-  render(){  
+  render() {
     const { gifs, fgifs } = this.state;
-    return !gifs.length ?
-      (<div className="App tc">
-        <h1 className="f1">Gif Search Engine</h1> 
-      <SearchBox searchChange={this.onSearchChange}/>
-      <Scroll>
-      <DisplayGif fgifs={fgifs}/>
-      </Scroll>
+    return !gifs.length ? (
+      <div className="App tc">
+        <h1 className="f1">Gif Search Engine</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <DisplayGif fgifs={fgifs} />
+        </Scroll>
       </div>
-      ) : 
-        (
-        <div className="App tc">
-          <h1 className="f1">Gif Search Engine</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
-          <Scroll>
-            <ErrorBoundary>
-              <GifList gifs={gifs}/>
-            </ErrorBoundary>  
-          </Scroll>
-        </div>
-      );
+    ) : (
+      <div className="App tc">
+        <h1 className="f1">Gif Search Engine</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <ErrorBoundary>
+            <GifList gifs={gifs} />
+          </ErrorBoundary>
+        </Scroll>
+      </div>
+    );
   }
 }
 
